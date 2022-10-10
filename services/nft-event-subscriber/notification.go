7e8cd8b720d6d926a1c8adb9b9b82b1598c0e0ea
minhkstn <minhnguyenkstn@gmail.com>
2022-10-10 15:38:21 +0200

    Delete print function

--- services/nft-event-subscriber/notification.go
+++ services/nft-event-subscriber/notification.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 
 	notification "github.com/bitmark-inc/autonomy-notification"
@@ -10,7 +8,6 @@ import (
 
 // notifyNewNFT notifies the arrival of a new token
 func (s *NFTEventSubscriber) notifyNewNFT(accountID, toAddress, tokenID string) error {
-	fmt.Println("\n\n ==> notifyNew tokenID: ", tokenID)
 	return s.notification.SendNotification("",
 		notification.NEW_NFT_ARRIVED,
 		accountID,
