1b271d9346bc5ab135ddd78f22f05173cb4c9676
Phuc Duong <114142495+PhucDuongBitmark@users.noreply.github.com>
2023-01-12 08:22:30 +0700

    [command] replace hic et nunc default thumbnail by Objkt CDN URL (#74)
    * [command] fix hic et nunc default thumb code

---
+++ command/hen_default_thumb_to_objkt_CDN/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/bitmark-inc/config-loader"
+	indexer "github.com/bitmark-inc/nft-indexer"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"strings"
+)
+
+type NFTAsset struct {
+	IndexID string `bson:"indexID"`
+}
+
+func main() {
+	var asset NFTAsset
+	var indexIDs []string
+
+	config.LoadConfig("NFT_INDEXER")
+	ctx := context.Background()
+
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(viper.GetString("store.db_uri")))
+	if err != nil {
+		panic(err)
+	}
+
+	db := mongoClient.Database(viper.GetString("store.db_name"))
+	assetsCollection := db.Collection("assets")
+
+	cursor, err := assetsCollection.Find(ctx, bson.M{
+		"$and": bson.A{
+			bson.M{"$or": bson.A{
+				bson.M{"projectMetadata.latest.thumbnailURL": bson.M{"$regex": "QmNrhZHUaEqxhyLfqoq1mtHSipkWHeT31LNHb1QEbDHgnc"}},
+				bson.M{"projectMetadata.latest.galleryThumbnailURL": bson.M{"$regex": "QmNrhZHUaEqxhyLfqoq1mtHSipkWHeT31LNHb1QEbDHgnc"}},
+			}},
+			bson.M{"$or": bson.A{
+				bson.M{"indexID": bson.M{"$regex": "tez"}},
+			}},
+		},
+	})
+
+	if err != nil {
+		defer cursor.Close(ctx)
+		panic(err)
+	}
+
+	for cursor.Next(ctx) {
+		err := cursor.Decode(&asset)
+		if err != nil {
+			panic(err)
+		}
+		indexIDs = append(indexIDs, asset.IndexID)
+	}
+
+	cursor.Close(ctx)
+
+	ad := indexer.AssetMetadataDetail{}
+
+	for _, indexID := range indexIDs {
+		s := strings.Split(indexID, "-")
+		thumbnailCDN := ad.ReplaceIPFSURIByObjktCDNURI(indexer.ObjktCDNArtifactThumbnailType, "ipfs://", s[1], s[2])
+
+		if thumbnailCDN == "ipfs://" {
+			thumbnailCDN = strings.ReplaceAll(indexer.DEFAULT_DISPLAY_URI, "ipfs://", indexer.DEFAULT_IPFS_GATEWAY)
+		}
+
+		_, err := assetsCollection.UpdateOne(
+			ctx,
+			bson.M{"indexID": bson.M{"$eq": indexID}},
+			bson.M{"$set": bson.M{
+				"projectMetadata.latest.thumbnailURL":        thumbnailCDN,
+				"projectMetadata.latest.galleryThumbnailURL": thumbnailCDN,
+			}},
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("update thumbnail for asset have indexID: ", indexID)
+	}
+}
