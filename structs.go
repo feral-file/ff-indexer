fefe6c5188ad804a1de65e9adc2c35005169930e
hvthhien <hvthhien@gmail.com>
2023-02-22 10:09:34 +0700

    update typo

--- structs.go
+++ structs.go
@@ -80,7 +80,7 @@ type Token struct {
 	Provenances       []Provenance `json:"provenance" bson:"provenance"`
 	LastActivityTime  time.Time    `json:"lastActivityTime" bson:"lastActivityTime"`
 	LastRefreshedTime time.Time    `json:"lastRefreshedTime" bson:"lastRefreshedTime"`
-	LastUpdatedAt     time.Time    `json:"LastUpdatedAt" bson:"LastUpdatedAt"`
+	LastUpdatedAt     time.Time    `json:"lastUpdatedAt" bson:"lastUpdatedAt"`
 }
 
 type AssetAttributes struct {
