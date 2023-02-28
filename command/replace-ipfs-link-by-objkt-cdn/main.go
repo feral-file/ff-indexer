fa1173c9403aad963f0f95784a61a75b90f18acd
Duong Phuc <>
2023-02-28 11:41:02 +0700

    command replace IPFS link with objkt cdn

---
+++ command/replace-ipfs-link-by-objkt-cdn/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/bitmark-inc/config-loader"
+	indexer "github.com/bitmark-inc/nft-indexer"
+	"github.com/bitmark-inc/nft-indexer/externals/objkt"
+	"github.com/bitmark-inc/nft-indexer/externals/tzkt"
+	"github.com/spf13/viper"
+)
+
+type NFTAsset struct {
+	ID              string                           `bson:"id"`
+	IndexID         string                           `bson:"indexID"`
+	ProjectMetadata indexer.VersionedProjectMetadata `bson:"projectMetadata"`
+}
+
+func main() {
+	var asset NFTAsset
+	var assetMetadataDetail indexer.AssetMetadataDetail
+	engine := indexer.New("", nil, tzkt.New(""), nil, objkt.New("https://data.objkt.com/v3/graphql"))
+
+	config.LoadConfig("NFT_INDEXER")
+	ctx := context.Background()
+
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(viper.GetString("store.db_uri")))
+	if err != nil {
+		panic(err)
+	}
+
+	db := mongoClient.Database(viper.GetString("store.db_name"))
+	assetsCollection := db.Collection("assets")
+	tokenCollection := db.Collection("tokens")
+
+	cursor, err := assetsCollection.Find(ctx, bson.M{"$or": bson.A{
+		bson.M{"projectMetadata.latest.thumbnailURL": bson.M{"$regex": "https://ipfs.io/ipfs/"}},
+		bson.M{"projectMetadata.latest.previewURL": bson.M{"$regex": "https://ipfs.io/ipfs/"}},
+	}})
+
+	if err != nil {
+		panic(err)
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		err = cursor.Decode(&asset)
+		if err != nil {
+			panic(err)
+		}
+
+		// get token by assetID
+		var token indexer.Token
+
+		err = tokenCollection.FindOne(
+			ctx,
+			bson.M{
+				"assetID": asset.ID,
+			},
+		).Decode(&token)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// get contract and token id from token indexID
+		s := strings.Split(token.IndexID, "-")
+		contract := s[1]
+		tokenID := s[2]
+
+		// get objkt cdn
+		objktToken, err := engine.GetObjktToken(contract, tokenID)
+		if err != nil {
+			fmt.Println(err)
+			if strings.Contains(fmt.Sprint(err), "there is no token in objkt") {
+				continue
+			}
+
+			panic(err)
+		}
+
+		assetMetadataDetail.FromObjkt(objktToken)
+
+		// replace asset thumbnail url
+		_, err = assetsCollection.UpdateOne(
+			ctx,
+			bson.M{"id": asset.ID},
+			bson.M{
+				"$set": bson.M{
+					"projectMetadata.latest.previewURL":          assetMetadataDetail.PreviewURI,
+					"projectMetadata.latest.thumbnailURL":        assetMetadataDetail.DisplayURI,
+					"projectMetadata.latest.galleryThumbnailURL": assetMetadataDetail.DisplayURI,
+				},
+			},
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("update asset url for asset ID: ", asset.ID)
+	}
+}
