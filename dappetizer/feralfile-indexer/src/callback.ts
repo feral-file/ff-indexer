7c87c93773340f358d691afdc4a5cdc3cc37d24b
Jim Yeh <jim@bitmark.com>
2023-03-01 19:00:35 +0900

    Support reporting tezos events via grpc (#83)
    * Adopt grpc to tezos-nft-indexer
    * Update the deployment and variable names
    * Fix rpc node in configmap
    * Fix the name alias by PR
    * Update the deployment for tezos-emitter

--- dappetizer/feralfile-indexer/src/callback.ts
+++ dappetizer/feralfile-indexer/src/callback.ts
@@ -6,6 +6,10 @@ import {
 } from '@tezos-dappetizer/indexer';
 import axios from 'axios';
 
+import { EventProcessorClient } from './event-processor_grpc_pb'
+import { EventInput } from './event-processor_pb'
+import * as grpc from "@grpc/grpc-js";
+
 interface TransferParameterItemTxsItem {
   /** Nat - arbitrary big integer >= 0. */
   amount: BigNumber;
@@ -23,8 +27,22 @@ interface TezosTransferParameterItem {
 
 const IS_TESTNET = <string>process.env.NETWORK == "testnet"
 
+const eventProcessorURI = <string>process.env.EVENT_PROCESSOR_URI
+const eventSubscriberURL = <string>process.env.EVENT_SUBSCRIBER_URL
+
+if (!eventSubscriberURL) {
+  console.log("[TEZOS_EMITTER]", "event subscriber url not set")
+}
+
+var grpcClient: EventProcessorClient
+if (eventProcessorURI) {
+  grpcClient = new EventProcessorClient(eventProcessorURI, grpc.ChannelCredentials.createInsecure());
+} else {
+  console.log("[TEZOS_EMITTER]", "event processor uri not set")
+}
+
 async function reportEvent(timestamp: Date, contract: string, tokenID: string, from_: string, to_: string) {
-  await axios.post(<string>process.env.EVENT_PROCESSOR_URL, {
+  await axios.post(eventSubscriberURL, {
     "timestamp": timestamp,
     "contract": contract,
     "tokenID": tokenID,
@@ -34,36 +52,92 @@ async function reportEvent(timestamp: Date, contract: string, tokenID: string, f
   })
 }
 
-export function outputTransferStdout(parameter: TezosTransferParameterItem[], indexingContext: TransactionIndexingContext) {
-  {
-    parameter.forEach((transfer => {
-      transfer.txs.forEach(async function (items) {
-        try {
-          console.log("[TOKEN_TRANSFER]", "(", indexingContext.contract.address, ")",
-            "id:", items.token_id.toFixed(), "from:", transfer.from_, "to:", items.to_)
-        } catch (error) {
-          console.log("fail to push event", error)
-          throw error
-        }
-      })
-    }))
+async function grpcReportEvent(timestamp: Date, contract: string, tokenID: string, from_: string, to_: string) {
+  if (!grpcClient) {
+    throw Error("grpc client is not initialized")
   }
+
+  let event = new EventInput()
+  event.setBlockchain("tezos")
+  event.setType("transfer")
+  event.setContract(contract)
+  event.setFrom(from_)
+  event.setTo(to_)
+  event.setTokenid(tokenID)
+  await new Promise((resolve, reject) => {
+    grpcClient.pushEvent(event, (error, response) => {
+      if (error) {
+        reject(error);
+        return
+      }
+
+      let respStatus = response.getStatus()
+      if (respStatus == 200) {
+        resolve("")
+      } else {
+        reject(response.getResult());
+      }
+    })
+  })
+}
+
+export function outputTransferStdout(parameter: TezosTransferParameterItem[], indexingContext: TransactionIndexingContext) {
+  parameter.forEach((transfer => {
+    transfer.txs.forEach(async function (items) {
+      try {
+        console.log("[TOKEN_TRANSFER]", "<STDOUT>", "(", indexingContext.contract.address, ")",
+          "id:", items.token_id.toFixed(), "from:", transfer.from_, "to:", items.to_)
+      } catch (error) {
+        console.log("fail to push event", error)
+        throw error
+      }
+    })
+  }))
+}
+
+export function outputTransferGRPC(parameter: TezosTransferParameterItem[], indexingContext: TransactionIndexingContext) {
+  parameter.forEach((transfer => {
+    transfer.txs.forEach(async function (items) {
+      try {
+        await grpcReportEvent(
+          indexingContext.block.timestamp, indexingContext.contract.address,
+          items.token_id.toFixed(), transfer.from_, items.to_)
+        console.log("[TOKEN_TRANSFER]", "<GRPC>", "(", indexingContext.contract.address, ")",
+          "id:", items.token_id.toFixed(), "from:", transfer.from_, "to:", items.to_)
+      } catch (error) {
+        console.log("fail to push event through grpc", error)
+      }
+    })
+  }))
+}
+
+
+export function outputTransferAPI(parameter: TezosTransferParameterItem[], indexingContext: TransactionIndexingContext) {
+  parameter.forEach((transfer => {
+    transfer.txs.forEach(async function (items) {
+      try {
+        await reportEvent(
+          indexingContext.block.timestamp, indexingContext.contract.address,
+          items.token_id.toFixed(), transfer.from_, items.to_)
+        console.log("[TOKEN_TRANSFER]", "<API>", "(", indexingContext.contract.address, ")",
+          "id:", items.token_id.toFixed(), "from:", transfer.from_, "to:", items.to_)
+      } catch (error) {
+        console.log("fail to push event through api", error)
+      }
+    })
+  }))
 }
 
 export function outputTransfer(parameter: TezosTransferParameterItem[], indexingContext: TransactionIndexingContext) {
-  {
-    parameter.forEach((transfer => {
-      transfer.txs.forEach(async function (items) {
-        try {
-          await reportEvent(
-            indexingContext.block.timestamp, indexingContext.contract.address,
-            items.token_id.toFixed(), transfer.from_, items.to_)
-          console.log("[TOKEN_TRANSFER]", "(", indexingContext.contract.address, ")",
-            "id:", items.token_id.toFixed(), "from:", transfer.from_, "to:", items.to_)
-        } catch (error) {
-          console.log("fail to push event", error)
-        }
-      })
-    }))
+  if (eventProcessorURI) {
+    outputTransferGRPC(parameter, indexingContext)
+  }
+
+  if (eventSubscriberURL) {
+    outputTransferAPI(parameter, indexingContext)
+  }
+
+  if (!eventProcessorURI && !eventSubscriberURL) {
+    outputTransferStdout(parameter, indexingContext)
   }
 }
