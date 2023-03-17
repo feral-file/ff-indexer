4e43af9b86f4cbc6f09bc5b8018441061b4a22a0
Duong Phuc <>
2023-03-17 14:51:44 +0700

    structure gRPC Indexer Client functions

--- sdk/nft-indexer-grpc/client.go
+++ sdk/nft-indexer-grpc/client.go
@@ -3,6 +3,7 @@ package sdk
 import (
 	"context"
 	"google.golang.org/grpc/credentials/insecure"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -15,6 +16,7 @@ type IndexerGRPCClient struct {
 	mapper *Mapper
 }
 
+// NewIndexerClient returns a new IndexerGRPCClient
 func NewIndexerClient(ServerURL string) (*IndexerGRPCClient, error) {
 	conn, err := grpc.Dial(ServerURL, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -29,6 +31,7 @@ func NewIndexerClient(ServerURL string) (*IndexerGRPCClient, error) {
 	}, nil
 }
 
+// GetTokensByIndexID returns a token by indexID
 func (i *IndexerGRPCClient) GetTokensByIndexID(ctx context.Context, indexID string) (*indexer.Token, error) {
 	token, err := i.client.GetTokensByIndexID(ctx, &pb.IndexID{IndexID: indexID})
 	if err != nil {
@@ -36,4 +39,29 @@ func (i *IndexerGRPCClient) GetTokensByIndexID(ctx context.Context, indexID stri
 	}
 
 	return i.mapper.MapGrpcTokenToIndexerToken(token), nil
+}
+
+// PushProvenance pushes provenance to indexer db
+func (i *IndexerGRPCClient) PushProvenance(ctx context.Context, indexID string, lockedTime time.Time, provenance indexer.Provenance) error {
+	return nil
+}
+
+// UpdateOwner updates owner of a token
+func (i *IndexerGRPCClient) UpdateOwner(ctx context.Context, indexID, owner string, updatedAt time.Time) error {
+	return nil
+}
+
+// UpdateOwnerForFungibleToken updates owner of a fungible token
+func (i *IndexerGRPCClient) UpdateOwnerForFungibleToken(ctx context.Context, indexID string, lockedTime time.Time, to string, total int64) error {
+	return nil
+}
+
+// IndexAccountTokens indexes account tokens
+func (i *IndexerGRPCClient) IndexAccountTokens(ctx context.Context, owner string, accountTokens []indexer.AccountToken) error {
+	return nil
+}
+
+// GetDetailedToken returns a detailed token by indexID
+func (i *IndexerGRPCClient) GetDetailedToken(ctx context.Context, indexID string) (indexer.DetailedToken, error) {
+	return indexer.DetailedToken{}, nil
 }
--- sdk/nft-indexer-grpc/client_test.go
+++ sdk/nft-indexer-grpc/client_test.go
@@ -3,11 +3,14 @@ package sdk
 import (
 	"context"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetTokensByIndexID(t *testing.T) {
+	indexID := "tez-KT1VZ6Zkoae9DtXkbuw4wtFCg9WH8eywcvEX-23798030035473632618901897089878275372960165372586891230635421889000008911882"
+
 	client, err := NewIndexerClient("localhost:8889")
 	if err != nil {
 		fmt.Println(err)
@@ -16,8 +19,8 @@ func TestGetTokensByIndexID(t *testing.T) {
 
 	token, err := client.GetTokensByIndexID(
 		context.Background(),
-		"tez-KT1VZ6Zkoae9DtXkbuw4wtFCg9WH8eywcvEX-23798030035473632618901897089878275372960165372586891230635421889000008911882"
-		)
+		indexID,
+	)
 
 	fmt.Println("token: ", token)
 	assert.NoError(t, err)
