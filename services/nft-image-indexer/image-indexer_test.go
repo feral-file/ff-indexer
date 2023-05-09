49b68d87f464f947e5d1f5e77a5751180cd9e9b4
Jim Yeh <jim@bitmark.com>
2023-05-09 14:35:14 +0800

    Fix test cases

--- services/nft-image-indexer/image-indexer_test.go
+++ services/nft-image-indexer/image-indexer_test.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
+	"github.com/bitmark-inc/nft-indexer/log"
 	imageStore "github.com/bitmark-inc/nft-indexer/services/nft-image-indexer/store"
 	"github.com/bitmark-inc/nft-indexer/services/nft-image-indexer/utils"
-	"github.com/stretchr/testify/assert"
 )
+
+func TestMain(m *testing.M) {
+	if err := log.Initialize("", false); err != nil {
+		panic(fmt.Errorf("fail to initialize logger with error: %s", err.Error()))
+	}
+	os.Exit(m.Run())
+}
 
 func TestDownloadFile(t *testing.T) {
 	url := "https://openseauserdata.com/files/dccefc262d24e3c43c1421efbc0e56f1.svg"
