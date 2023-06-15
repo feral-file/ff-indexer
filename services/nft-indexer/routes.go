e6ce646dddc41f3088db875705c1ba7c39306d64
Hien Ho <hvthhien@gmail.com>
2023-06-15 12:39:53 +0700

    API for index history (#188)
    * API for index history
    * add binding required for indexID

--- services/nft-indexer/routes.go
+++ services/nft-indexer/routes.go
@@ -63,6 +63,7 @@ func (s *NFTIndexerServer) SetupRoute() {
 	v2NFT.GET("", s.GetAccountNFTsV2)
 	v2NFT.POST("/query", s.QueryNFTsV2)
 	v2NFT.POST("/index_one", s.IndexOneNFT)
+	v2NFT.POST("/index_history", s.IndexHistory)
 
 	v2.POST("/graphql", s.graphqlHandler)
 	v2.GET("/graphiql", s.playgroundHandler)
