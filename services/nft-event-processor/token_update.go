7439a7ff16be4c026e8c57cc79b52c0a39be598e
Hien Ho <hvthhien@gmail.com>
2023-09-26 14:51:16 +0700

    listen bigmap updates event on tezos (#238)
    * listen bigmap updates event on tezos
    * event processor token updated check existent
    * maintain only 1 lastStoppedBlock in parameter store

--- services/nft-event-processor/token_update.go
+++ services/nft-event-processor/token_update.go
@@ -4,12 +4,35 @@ import (
 	"context"
 
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/status"
 
+	log "github.com/bitmark-inc/autonomy-logger"
+	indexer "github.com/bitmark-inc/nft-indexer"
 	indexerWorker "github.com/bitmark-inc/nft-indexer/background/worker"
 )
 
 func (e *EventProcessor) refreshTokenData(ctx context.Context, event NFTEvent) error {
-	indexerWorker.StartIndexTokenWorkflow(ctx, e.worker, event.To, event.Contract, event.TokenID, true, false)
+	blockchain := event.Blockchain
+	contract := event.Contract
+	tokenID := event.TokenID
+	indexID := indexer.TokenIndexID(blockchain, contract, tokenID)
+
+	token, err := e.indexerGRPC.GetTokenByIndexID(ctx, indexID)
+	if err != nil {
+		if grpcError, ok := status.FromError(err); !ok || grpcError.Message() != "token does not exist" {
+			log.Error("fail to query token from indexer", zap.Error(err))
+			return err
+		}
+	}
+
+	// check if a token is existent
+	// if existent, update the token
+	// if not, ignore the process
+	if token != nil {
+		indexerWorker.StartIndexTokenWorkflow(ctx, e.worker, event.To, event.Contract, event.TokenID, true, false)
+	}
+
 	return nil
 }
 
