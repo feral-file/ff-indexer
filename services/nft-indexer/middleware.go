fef94aaa852a65e723815b646163ba545a528035
Jim Yeh <jim@bitmark.com>
2023-10-30 15:51:58 +0800

    Remove mime type suggestion and asset change detection workflow

--- services/nft-indexer/middleware.go
+++ services/nft-indexer/middleware.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,45 +21,45 @@ func TokenAuthenticate(tokenKey, tokenValue string) gin.HandlerFunc {
 
 // authenticate validates a JWT signed from account server. It uses the shared rsa public key
 // to validate the token is signed from account server
-func (s *NFTIndexerServer) authenticate(c *gin.Context) {
-	tokenStrings := strings.Split(c.GetHeader("Authorization"), " ")
-	if len(tokenStrings) != 2 {
-		abortWithError(c, http.StatusForbidden, "invalid authorization format", nil)
-		return
-	}
+// func (s *NFTIndexerServer) authenticate(c *gin.Context) {
+// 	tokenStrings := strings.Split(c.GetHeader("Authorization"), " ")
+// 	if len(tokenStrings) != 2 {
+// 		abortWithError(c, http.StatusForbidden, "invalid authorization format", nil)
+// 		return
+// 	}
 
-	authMethod := tokenStrings[0]
-	if authMethod != "Bearer" {
-		abortWithError(c, http.StatusForbidden, "invalid authorization format", nil)
-		return
-	}
+// 	authMethod := tokenStrings[0]
+// 	if authMethod != "Bearer" {
+// 		abortWithError(c, http.StatusForbidden, "invalid authorization format", nil)
+// 		return
+// 	}
 
-	bearerToken := tokenStrings[1]
-	var claims PlanJWTClaim
-	token, err := jwt.ParseWithClaims(bearerToken, &claims, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
+// 	bearerToken := tokenStrings[1]
+// 	var claims PlanJWTClaim
+// 	token, err := jwt.ParseWithClaims(bearerToken, &claims, func(token *jwt.Token) (interface{}, error) {
+// 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+// 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+// 		}
 
-		return s.jwtPubkey, nil
-	})
+// 		return s.jwtPubkey, nil
+// 	})
 
-	if err != nil {
-		abortWithError(c, http.StatusForbidden, "error invalid token", err)
-		return
-	}
+// 	if err != nil {
+// 		abortWithError(c, http.StatusForbidden, "error invalid token", err)
+// 		return
+// 	}
 
-	if !token.Valid {
-		abortWithError(c, http.StatusForbidden, "error invalid token", nil)
-		return
-	}
+// 	if !token.Valid {
+// 		abortWithError(c, http.StatusForbidden, "error invalid token", nil)
+// 		return
+// 	}
 
-	requester := claims.Subject
-	if requester == "" {
-		abortWithError(c, http.StatusForbidden, "error invalid requester", nil)
-		return
-	}
-	c.Set("requester", requester)
-	c.Set("plan", string(claims.Plan))
-	c.Next()
-}
+// 	requester := claims.Subject
+// 	if requester == "" {
+// 		abortWithError(c, http.StatusForbidden, "error invalid requester", nil)
+// 		return
+// 	}
+// 	c.Set("requester", requester)
+// 	c.Set("plan", string(claims.Plan))
+// 	c.Next()
+// }
