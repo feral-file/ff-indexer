ca559c0fe57451b1beb9a38dfc51b0b02beaf7f7
hvthhien <hvthhien@gmail.com>
2024-03-27 17:35:53 +0700

    add configs

--- services/nft-event-processor-tezos-emitter/main.go
+++ services/nft-event-processor-tezos-emitter/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -20,6 +21,15 @@ func main() {
 	config.LoadConfig("NFT_INDEXER")
 	if err := log.Initialize(viper.GetString("log.level"), viper.GetBool("debug")); err != nil {
 		panic(fmt.Errorf("fail to initialize logger with error: %s", err.Error()))
+	}
+
+	environment := viper.GetString("environment")
+
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn:         viper.GetString("sentry.dsn"),
+		Environment: environment,
+	}); err != nil {
+		log.Panic("Sentry initialization failed", zap.Error(err))
 	}
 
 	ctx := context.Background()
