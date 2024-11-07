5bf92a1ea84e86c4ba447d2a3536278d8b464fcb
Hiếu Phạm <hieupham@bitmark.com>
2024-11-07 14:41:50 +0700

    fix wrong file download handling

--- services/nft-image-indexer/downloader.go
+++ services/nft-image-indexer/downloader.go
@@ -21,6 +21,10 @@ func DownloadFile(url string) (io.Reader, string, int, error) {
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, "", 0, fmt.Errorf("status code: %d", resp.StatusCode)
+	}
+
 	fileHeader := make([]byte, 512)
 	if n, err := resp.Body.Read(fileHeader); err != nil {
 		if errors.Is(err, io.EOF) {
