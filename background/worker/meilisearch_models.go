9c397fe5c2bbbd2f44c90ee87b22eed0a17dc55a
Anh Nguyen <anhnguyen@bitmark.com>
2025-09-04 16:32:55 +0700

    remove some fields

--- background/worker/meilisearch_models.go
+++ background/worker/meilisearch_models.go
@@ -7,41 +7,21 @@ import (
 )
 
 type MeilisearchTokenDocument struct {
-	IndexID           string                 `json:"indexID"`
-	TokenID           string                 `json:"tokenID"`
-	ContractAddress   string                 `json:"contractAddress"`
-	Blockchain        string                 `json:"blockchain"`
-	ContractType      string                 `json:"contractType"`
-	OwnerAddresses    []string               `json:"ownerAddresses"`
-	OwnerBalances     map[string]int64       `json:"ownerBalances"`
-	TotalSupply       int64                  `json:"totalSupply"`
-	Fungible          bool                   `json:"fungible"`
-	Title             string                 `json:"title"`
-	Description       string                 `json:"description"`
-	ArtistName        string                 `json:"artistName"`
-	ArtistID          string                 `json:"artistID"`
-	CollectionName    string                 `json:"collectionName,omitempty"`
-	Medium            string                 `json:"medium"`
-	MimeType          string                 `json:"mimeType"`
-	AssetURL          string                 `json:"assetURL"`
-	ThumbnailURL      string                 `json:"thumbnailURL"`
-	PreviewURL        string                 `json:"previewURL"`
-	ExternalURL       string                 `json:"externalURL"`
-	Attributes        map[string]interface{} `json:"attributes"`
-	Tags              []string               `json:"tags"`
-	Source            string                 `json:"source"`
-	Edition           int64                  `json:"edition"`
-	EditionName       string                 `json:"editionName"`
-	MaxEdition        int64                  `json:"maxEdition"`
-	MintedAt          time.Time              `json:"mintedAt"`
-	LastActivityTime  time.Time              `json:"lastActivityTime"`
-	LastRefreshedTime time.Time              `json:"lastRefreshedTime"`
-	IndexedAt         time.Time              `json:"indexedAt"`
-	Burned            bool                   `json:"burned"`
-	Swapped           bool                   `json:"swapped"`
-	BaseCurrency      string                 `json:"baseCurrency,omitempty"`
-	BasePrice         float64                `json:"basePrice,omitempty"`
-	SearchText        string                 `json:"searchText"`
+	IndexID           string                  `json:"indexID"`
+	Blockchain        string                  `json:"blockchain"`
+	ContractType      string                  `json:"contractType"`
+	Fungible          bool                    `json:"fungible"`
+	Title             string                  `json:"title"`
+	Description       string                  `json:"description"`
+	ArtistName        string                  `json:"artistName"`
+	CollectionName    string                  `json:"collectionName,omitempty"`
+	Medium            string                  `json:"medium"`
+	MimeType          string                  `json:"mimeType"`
+	Source            string                  `json:"source"`
+	MintedAt          time.Time               `json:"mintedAt"`
+	LastActivityTime  time.Time               `json:"lastActivityTime"`
+	LastRefreshedTime time.Time               `json:"lastRefreshedTime"`
+	FullToken         indexer.DetailedTokenV2 `json:"fullToken"`
 }
 
 type MeilisearchStreamConfig struct {
@@ -95,67 +75,22 @@ type MeilisearchBatchResult struct {
 func toMeilisearchDocument(token indexer.DetailedTokenV2) MeilisearchTokenDocument {
 	doc := MeilisearchTokenDocument{
 		IndexID:           token.IndexID,
-		TokenID:           token.ID,
-		ContractAddress:   token.ContractAddress,
 		Blockchain:        token.Blockchain,
 		ContractType:      token.ContractType,
 		Fungible:          token.Fungible,
-		OwnerAddresses:    []string{token.Owner},
-		OwnerBalances:     map[string]int64{token.Owner: token.Balance},
-		TotalSupply:       token.Balance,
 		Title:             token.Asset.Metadata.Project.Latest.Title,
 		Description:       token.Asset.Metadata.Project.Latest.Description,
 		ArtistName:        token.Asset.Metadata.Project.Latest.ArtistName,
-		ArtistID:          token.Asset.Metadata.Project.Latest.ArtistID,
 		Medium:            string(token.Asset.Metadata.Project.Latest.Medium),
 		MimeType:          token.Asset.Metadata.Project.Latest.MIMEType,
-		AssetURL:          token.Asset.Metadata.Project.Latest.AssetURL,
-		ThumbnailURL:      token.Asset.Metadata.Project.Latest.ThumbnailURL,
-		PreviewURL:        token.Asset.Metadata.Project.Latest.PreviewURL,
-		ExternalURL:       token.Asset.Metadata.Project.Latest.SourceURL,
 		Source:            token.Asset.Metadata.Project.Latest.Source,
 		Edition:           token.Edition,
 		EditionName:       token.EditionName,
-		MaxEdition:        token.Asset.Metadata.Project.Latest.MaxEdition,
 		MintedAt:          token.MintedAt,
 		LastActivityTime:  token.LastActivityTime,
 		LastRefreshedTime: token.LastRefreshedTime,
 		IndexedAt:         time.Now(),
-		Burned:            token.Burned,
-		Swapped:           token.Swapped,
-		BaseCurrency:      token.Asset.Metadata.Project.Latest.BaseCurrency,
-		BasePrice:         token.Asset.Metadata.Project.Latest.BasePrice,
+		FullToken:         token,
 	}
-	if token.Fungible && len(token.Owners) > 0 {
-		doc.OwnerAddresses = make([]string, 0, len(token.Owners))
-		doc.OwnerBalances = make(map[string]int64)
-		totalSupply := int64(0)
-		for address, balance := range token.Owners {
-			doc.OwnerAddresses = append(doc.OwnerAddresses, address)
-			doc.OwnerBalances[address] = balance
-			totalSupply += balance
-		}
-		doc.TotalSupply = totalSupply
-	}
-	if token.Asset.Attributes != nil {
-		doc.Attributes = make(map[string]interface{})
-	}
-	doc.SearchText = buildSearchText(doc)
 	return doc
-}
-
-func buildSearchText(doc MeilisearchTokenDocument) string {
-	parts := []string{doc.Title, doc.Description, doc.ArtistName, doc.CollectionName, doc.Medium}
-	parts = append(parts, doc.Tags...)
-	var result string
-	for _, part := range parts {
-		if part == "" {
-			continue
-		}
-		if result != "" {
-			result += " "
-		}
-		result += part
-	}
-	return result
 }
